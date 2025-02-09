51a5112480d27377a98cc19e2a2094053c643a49
TopeAanu <topefasasi99@gmail.com>
2025-02-09 23:04:25 +0100

    linking between page

--- app/news/page.js
+++ app/news/page.js
@@ -1,5 +1,17 @@
-// our-domain.com/
+import { Fragment } from "react";
 
 export default function NewsPage() {
-  return <h1>News Page</h1>
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <a href='/news/nextjs-is-a-great-framework'>Nextjs is really cool😎</a>
+        </li>
+        <li>
+          Something else
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -7,20 +7,7 @@ export default function Home() {
     <div className={styles.page}>
       <h1>Hi</h1>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        
       </main>
     </div>
   );
